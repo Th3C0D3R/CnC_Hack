eca07e6d6ed8b32b1844227b0f17076fe949ebd6
Th3C0D3R <nico.fengler@gmx.de>
2019-02-08 12:31:36 +0100

    updated sturcture

---
+++ CnC_Hack/ConsoleHelper.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnC_Hack
+{
+	class ConsoleHelper
+	{
+		public int WriteGameMenu(string[] choiceItems)
+		{
+			int curItem = 0, c;
+			ConsoleKeyInfo key;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("================ CnC Generals Hack V1 ================");
+				Console.WriteLine("");
+				// The loop that goes through all of the menu items.
+				for (c = 0; c < choiceItems.Length; c++)
+				{
+					if (curItem == c)
+					{
+						Console.Write(">>");
+						Console.WriteLine(choiceItems[c]);
+					}
+					else
+					{
+						Console.WriteLine(choiceItems[c]);
+					}
+				}
+				Console.WriteLine("");
+				Console.Write("Select your choice with the arrow keys.");
+				key = Console.ReadKey(true);
+				if (key.Key.ToString() == "DownArrow")
+				{
+					curItem++;
+					if (curItem > choiceItems.Length - 1) curItem = 0;
+				}
+				else if (key.Key.ToString() == "UpArrow")
+				{
+					curItem--;
+					if (curItem < 0) curItem = Convert.ToInt16(choiceItems.Length - 1);
+				}
+			} while (key.KeyChar != 13);
+			return curItem;
+		}
+		public int WriteHackMenu(string[] menuItems, bool hackActive, bool gubed)
+		{
+			int curItem = 0, c;
+			ConsoleKeyInfo key;
+			while (true)
+			{
+				do
+				{
+					Console.Clear();
+					Console.WriteLine("");
+					Console.WriteLine("================ CnC Generals Hack V1 ================");
+					Console.WriteLine("");
+					// The loop that goes through all of the menu items.
+					for (c = 0; c < menuItems.Length; c++)
+					{
+						if (hackActive)
+							menuItems[0] = "Stop Hack";
+						else
+							menuItems[0] = "Start Hack";
+						if (curItem == c)
+						{
+							if (!gubed) { if (menuItems[c] == "Debug") menuItems[c] = "Debug [Not Available]"; }
+							else { if (menuItems[c] == "Debug" || menuItems[c] == "Debug [Not Available]") menuItems[c] = "Debug"; }
+							Console.Write(">>");
+							Console.WriteLine(menuItems[c]);
+						}
+						else
+						{
+							if (!gubed) { if (menuItems[c] == "Debug") menuItems[c] = "Debug [Not Available]"; }
+							else { if (menuItems[c] == "Debug" || menuItems[c] == "Debug [Not Available]") menuItems[c] = "Debug"; }
+							Console.WriteLine(menuItems[c]);
+						}
+					}
+					Console.WriteLine("");
+					Console.Write("Select your choice with the arrow keys.");
+					key = Console.ReadKey(true);
+					if (key.Key.ToString() == "DownArrow")
+					{
+						curItem++;
+						if (curItem > menuItems.Length - 1) curItem = 0;
+					}
+					else if (key.Key.ToString() == "UpArrow")
+					{
+						curItem--;
+						if (curItem < 0) curItem = Convert.ToInt16(menuItems.Length - 1);
+					}
+				} while (key.KeyChar != 13);
+				return curItem;
+			}
+		}
+		public void WriteLog(StreamWriter sw, int gameModul, int[,][] Offsets)
+		{
+			sw.WriteLine("==== DEBUG LOG START ====");
+			sw.WriteLine("");
+			sw.WriteLine("");
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] GameModul Startaddress: 0x" + gameModul.ToString("X"));
+			sw.WriteLine("");
+			sw.WriteLine("");
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Offsets for Generals:");
+			sw.WriteLine("");
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] PlayBase: 0x" + Offsets[0, 0][0].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Money: 0x" + Offsets[0, 1][0].ToString("X") + " & 0x" + Offsets[0, 1][1].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Rank Points: 0x" + Offsets[0, 2][0].ToString("X") + " & 0x" + Offsets[0, 2][1].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Experience: 0x" + Offsets[0, 3][0].ToString("X") + " & 0x" + Offsets[0, 3][1].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Energy Used: 0x" + Offsets[0, 4][0].ToString("X") + " & 0x" + Offsets[0, 4][1].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Energy Buffer: 0x" + Offsets[0, 5][0].ToString("X") + " & 0x" + Offsets[0, 5][1].ToString("X"));
+			sw.WriteLine("");
+			sw.WriteLine("");
+			sw.WriteLine("");
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Offsets for Generals - Zero Hour:");
+			sw.WriteLine("");
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] PlayBase: 0x" + Offsets[1, 0][0].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Money: 0x" + Offsets[1, 1][0].ToString("X") + " & 0x" + Offsets[1, 1][1].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Rank Points: 0x" + Offsets[1, 2][0].ToString("X") + " & 0x" + Offsets[1, 2][1].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Experience: 0x" + Offsets[1, 3][0].ToString("X") + " & 0x" + Offsets[1, 3][1].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Energy Used: 0x" + Offsets[1, 4][0].ToString("X") + " & 0x" + Offsets[1, 4][1].ToString("X"));
+			sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Energy Buffer: 0x" + Offsets[1, 5][0].ToString("X") + " & 0x" + Offsets[1, 5][1].ToString("X"));
+			sw.WriteLine("");
+			sw.WriteLine("");
+			sw.WriteLine("==== DEBUG LOG END ====");
+			sw.WriteLine("");
+			sw.WriteLine("");
+			sw.WriteLine("");
+			sw.WriteLine("");
+		}
+	}
+}
--- CnC_Hack/Program.cs
+++ CnC_Hack/Program.cs
@@ -46,16 +46,14 @@ namespace CnC_Hack
 		static bool hackActive = false;
 		static bool isZeroHour = false;
 		static bool gubed = false;
-		static short curItem = 0, c;
-		static ConsoleKeyInfo key;
 		static string[] choiceItems = { "Generals", "Generals - Zero Hour" };
 		static string[] menuItems = { "Start Hack", "Change Game", "Debug", "Exit" };
 		static Process process;  //search value
 		static BackgroundWorker bwHack = new BackgroundWorker();
+		static ConsoleHelper ch = new ConsoleHelper();
 
 		static unsafe void Main(string[] args)
 		{
-
 			if (args.Length == 1)
 			{
 				if (new string(args[0].ToLower().ToCharArray().Reverse().ToArray()) == "gubed")
@@ -63,7 +61,6 @@ namespace CnC_Hack
 					gubed = true;
 				}
 			}
-
 			Console.WriteLine("Looking for Generals.exe...");
 			while (Process.GetProcessesByName("Generals").Count() == 0) { }
 			Console.WriteLine("Generals.exe found!");
@@ -112,125 +109,51 @@ namespace CnC_Hack
 		}
 		static public void RenderMenu()
 		{
-			while (true)
+			int item = ch.WriteHackMenu(menuItems, hackActive, gubed);
+			switch (item)
 			{
-				do
-				{
-					Console.Clear();
-					Console.WriteLine("");
-					Console.WriteLine("================ CnC Generals Hack V1 ================");
-					Console.WriteLine("");
-					// The loop that goes through all of the menu items.
-					for (c = 0; c < menuItems.Length; c++)
+				case 0:
+					hackActive = !hackActive;
+					if (hackActive)
 					{
-						if (hackActive)
-							menuItems[0] = "Stop Hack";
-						else
-							menuItems[0] = "Start Hack";
-						if (curItem == c)
-						{
-							if (!gubed) { if (menuItems[c] == "Debug") menuItems[c] = "Debug [Not Available]"; }
-							else { if (menuItems[c] == "Debug" || menuItems[c] == "Debug [Not Available]") menuItems[c] = "Debug"; }
-							Console.Write(">>");
-							Console.WriteLine(menuItems[c]);
-						}
-						else
-						{
-							if (!gubed) { if (menuItems[c] == "Debug") menuItems[c] = "Debug [Not Available]"; }
-							else { if (menuItems[c] == "Debug" || menuItems[c] == "Debug [Not Available]") menuItems[c] = "Debug"; }
-							Console.WriteLine(menuItems[c]);
-						}
+						hack(5000, Offsets[isZeroHour ? 1 : 0, 3][0], Offsets[isZeroHour ? 1 : 0, 3][1]); //RankEXP -> LevelUp to get StarRank
+						bwHack.RunWorkerAsync();
 					}
-					Console.WriteLine("");
-					Console.Write("Select your choice with the arrow keys.");
-					key = Console.ReadKey(true);
-					if (key.Key.ToString() == "DownArrow")
-					{
-						curItem++;
-						if (curItem > menuItems.Length - 1) curItem = 0; 
-					}
-					else if (key.Key.ToString() == "UpArrow")
-					{
-						curItem--;
-						if (curItem < 0) curItem = Convert.ToInt16(menuItems.Length - 1);
-					}
-				} while (key.KeyChar != 13);
-				switch (curItem)
-				{
-					case 0:
-						hackActive = !hackActive;
-						if (hackActive)
-						{
-							hack(5000, Offsets[isZeroHour ? 1 : 0, 3][0], Offsets[isZeroHour ? 1 : 0, 3][1]); //RankEXP -> LevelUp to get StarRank
-							bwHack.RunWorkerAsync();
-						}
-						break;
-					case 1:
-						int i = RenderChoice();
-						while (i == -1) { i = RenderChoice(); }
-						bwHack.CancelAsync();
-						break;
-					case 2:
-						if(gubed)
-							doDebug();
-						break;
-					case 3:
-						return;
-					default:
-						break;
-				}
+					break;
+				case 1:
+					int i = RenderChoice();
+					while (i == -1) { i = RenderChoice(); }
+					bwHack.CancelAsync();
+					break;
+				case 2:
+					if (gubed)
+						doDebug();
+					break;
+				case 3:
+					return;
+				default:
+					break;
 			}
 		}
 		static public int RenderChoice()
 		{
-			do
-			{
-				Console.Clear();
-				Console.WriteLine("================ CnC Generals Hack V1 ================");
-				Console.WriteLine("");
-				// The loop that goes through all of the menu items.
-				for (c = 0; c < choiceItems.Length; c++)
-				{
-					if (curItem == c)
-					{
-						Console.Write(">>");
-						Console.WriteLine(choiceItems[c]);
-					}
-					else
-					{
-						Console.WriteLine(choiceItems[c]);
-					}
-				}
-				Console.WriteLine("");
-				Console.Write("Select your choice with the arrow keys.");
-				key = Console.ReadKey(true);
-				if (key.Key.ToString() == "DownArrow")
-				{
-					curItem++;
-					if (curItem > choiceItems.Length - 1) curItem = 0;
-				}
-				else if (key.Key.ToString() == "UpArrow")
-				{
-					curItem--;
-					if (curItem < 0) curItem = Convert.ToInt16(choiceItems.Length - 1);
-				}
-			} while (key.KeyChar != 13);
-			switch (curItem)
+			int item = ch.WriteGameMenu(choiceItems);
+			switch (item)
 			{
 				case 0:
-					curItem = 0;
+					item = 0;
 					isZeroHour = false;
 					return 0;
 				case 1:
-					curItem = 0;
+					item = 0;
 					isZeroHour = true;
 					return 1;
 				default:
-					curItem = 0;
+					item = 0;
 					return -1;
 					break;
 			}
-			curItem = 0;
+			item = 0;
 			return -1;
 		}
 		private static byte[] StructureToByteArray(object obj)
@@ -259,38 +182,7 @@ namespace CnC_Hack
 				}
 				using (StreamWriter sw = new StreamWriter("debug.log", true))
 				{
-					sw.WriteLine("==== DEBUG LOG START ====");
-					sw.WriteLine("");
-					sw.WriteLine("");
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] GameModul Startaddress: 0x" + gameModul.ToString("X"));
-					sw.WriteLine("");
-					sw.WriteLine("");
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Offsets for Generals:");
-					sw.WriteLine("");
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] PlayBase: 0x" + Offsets[0, 0][0].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Money: 0x" + Offsets[0, 1][0].ToString("X") + " & 0x" + Offsets[0, 1][1].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Rank Points: 0x" + Offsets[0, 2][0].ToString("X") + " & 0x" + Offsets[0, 2][1].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Experience: 0x" + Offsets[0, 3][0].ToString("X") + " & 0x" + Offsets[0, 3][1].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Energy Used: 0x" + Offsets[0, 4][0].ToString("X") + " & 0x" + Offsets[0, 4][1].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Energy Buffer: 0x" + Offsets[0, 5][0].ToString("X") + " & 0x" + Offsets[0, 5][1].ToString("X"));
-					sw.WriteLine("");
-					sw.WriteLine("");
-					sw.WriteLine("");
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Offsets for Generals - Zero Hour:");
-					sw.WriteLine("");
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] PlayBase: 0x" + Offsets[1, 0][0].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Money: 0x" + Offsets[1, 1][0].ToString("X") + " & 0x" + Offsets[1, 1][1].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Rank Points: 0x" + Offsets[1, 2][0].ToString("X") + " & 0x" + Offsets[1, 2][1].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Experience: 0x" + Offsets[1, 3][0].ToString("X") + " & 0x" + Offsets[1, 3][1].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Energy Used: 0x" + Offsets[1, 4][0].ToString("X") + " & 0x" + Offsets[1, 4][1].ToString("X"));
-					sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] Energy Buffer: 0x" + Offsets[1, 5][0].ToString("X") + " & 0x" + Offsets[1, 5][1].ToString("X"));
-					sw.WriteLine("");
-					sw.WriteLine("");
-					sw.WriteLine("==== DEBUG LOG END ====");
-					sw.WriteLine("");
-					sw.WriteLine("");
-					sw.WriteLine("");
-					sw.WriteLine("");
+					ch.WriteLog(sw, gameModul, Offsets);
 				}
 
 			}
